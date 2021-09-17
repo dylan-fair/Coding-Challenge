669c2d6c749ca5b696a62fc72d7d11639c21d834
dylan fair <dylanfair11@gmail.com>
2021-09-16 20:06:59 -0500

    added working timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ var body = document.querySelector("#body")
 var header = document.querySelector("#header");
 var mainPage = document.createElement("div");
 var timer = 75;
+var questions = ["Which is a loop?", "How to end a line in javascript?", "Whats it called when you call a function inside itself?"];
+var answers = [["Wheel loop", "conditional loop", "for loop", "if loop"], [":", ".", ",", ";"], ["Loops", "Recursion", "science", "cool stuff"]];
+var answerKey = [2, 3, 1];
+var clock = document.createElement("h2");
 
 var frontPage = function() {
     var title = document.createElement("h1");
@@ -11,7 +15,7 @@ var frontPage = function() {
     title.textContent = "Coding Quiz";
     title.className = "title";
     mainPage.appendChild(title);
-    descript.textContent = "Welcome to the coding quiz! When you press start you will be presented with questions. Each wrong loses time! Once all complete your time is your high score! Good luck";
+    descript.textContent = "Welcome to the coding quiz! When you press start you will be presented with questions. Each wrong loses time! Once all complete your time is your high score! Good luck!";
     descript.className = "description";
     mainPage.appendChild(descript);
     start.textContent = "Start Quiz";
@@ -21,48 +25,53 @@ var frontPage = function() {
     body.appendChild(mainPage);
 };
 var time = function() {
-    var clock = document.createElement("h2");
     clock.textContent = "Time: " + timer;
     header.appendChild(clock);
 
 };
+var timeInterval = setInterval(function(){
+    if (timer <  0) {
+        return;
+    }
+    clock.textContent = "Time: " + timer;
+    timer--;
+
+}, 1000);
 var buttonHandler = function(event) {
     var targetEl = event.target;
     if (targetEl.matches(".start-btn")) {
         console.log("hello");
         mainPage.remove();
-        quiz();
+        quizHandler();
+        timeInterval();
 
     }
     else if (targetEl.matches(".btn")) {
         console.log("high score!");
     }
 }
-var quiz = function() {
-    var questions = ["Which is a loop?", "how to end a line in javascript?", "Whats it called when you call a function inside itself?"];
-    var answers = [["Wheel loop", "conditional loop", "for loop", "if loop"], [":", ".", ",", ";"], ["Loops", "Recursion", "science", "cool stuff"]];
-    var answerKey = [2, 3, 1];
+var quizHandler = function() {
+    for(var i = 0; i < questions.length; i++) {
+        quiz(questions[i], answers[i], answerKey[i]);
+    }
+}
+var quiz = function(q, a , ak) {
     var quizScreen = document.createElement("div");
     quizScreen.className = "quiz";
     var questionTitle = document.createElement("h1");
+    questionTitle.textContent = q;
     var answersButtons = document.createElement("ul");
     answersButtons.className = "answer-list";
-    for (var i = 0; i < questions.length; i++) {
-        var counter = 0;
-        questionTitle.textContent = questions[i];
-        quizScreen.appendChild(questionTitle);
-        for (var x = 0; x < 4; x++) {
-            var answerEl = document.createElement("li");
-            var answerbtn = document.createElement("button");
-            answerbtn.textContent = answers[i][x];
-            answerbtn.setAttribute("data-answer-id", counter);
-            answerEl.appendChild(answerbtn);
-            answersButtons.appendChild(answerEl);
-            counter++;
-        }
-        quizScreen.appendChild(answersButtons);
-        body.appendChild(quizScreen);
+    for (var i = 0; i < a.length; i++) {
+        var listEl = document.createElement("li");
+        var button = document.createElement("button");
+        button.textContent = a[i];
+        listEl.appendChild(button);
+        answersButtons.appendChild(listEl);
     }
+    quizScreen.appendChild(questionTitle);
+    quizScreen.appendChild(answersButtons);
+    body.appendChild(quizScreen);
 };
 time();
 frontPage();
