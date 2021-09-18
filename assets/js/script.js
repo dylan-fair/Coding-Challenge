614d460e6ee06140856d7164b68b6a6072b7ddaa
dylan fair <dylanfair11@gmail.com>
2021-09-18 02:51:39 -0500

    added high score screen and return button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,19 @@ var body = document.querySelector("#body")
 var header = document.querySelector("#header");
 var mainPage = document.createElement("div");
 var timer = 75;
-var questions = ["Which is a loop?", "How to end a line in javascript?", "Whats it called when you call a function inside itself?"];
-var answers = [["Wheel loop", "conditional loop", "for loop", "if loop"], [":", ".", ",", ";"], ["Loops", "Recursion", "science", "cool stuff"]];
-var answerKey = [2, 3, 1];
+var questions = ["Which is a loop?", "How to end a line in javascript?", "Whats it called when you call a function inside itself?", "Which is a var type?"];
+var answers = [["Wheel loop", "conditional loop", "for loop", "if loop"], [":", ".", ",", ";"], ["Loops", "Recursion", "science", "cool stuff"], ["Object", "Function", "Cout", "print"]];
+var answerKey = [2, 3, 1, 0];
 var clock = document.createElement("h2");
+var index = 0;
+var quizScreen = document.createElement("div");
+var finalScore = 0;
+var timeInt;
+var initials = [];
+var inputScreen = document.createElement("div");
+inputScreen.className = "inputScreen";
+var highScoreScreen = document.createElement("div");
+highScoreScreen.className = "high-score-screen";
 
 var frontPage = function() {
     var title = document.createElement("h1");
@@ -29,34 +38,72 @@ var time = function() {
     header.appendChild(clock);
 
 };
-var timeInterval = setInterval(function(){
-    if (timer <  0) {
-        return;
-    }
-    clock.textContent = "Time: " + timer;
-    timer--;
+var timeInterval = function() {
+    timeInt = setInterval(function(){
+        if (timer <  0) {
+            return;
+        }
+        clock.textContent = "Time: " + timer;
+        timer--;
+    
+    }, 1000);
+}
 
-}, 1000);
 var buttonHandler = function(event) {
     var targetEl = event.target;
+    console.log(targetEl);
     if (targetEl.matches(".start-btn")) {
-        console.log("hello");
         mainPage.remove();
-        quizHandler();
+        quiz(questions[index], answers[index]);
         timeInterval();
 
     }
+    else if (targetEl.matches("[data-answer-id]")) {
+        var id = targetEl.getAttribute("data-answer-id");
+        if(index === questions.length -1) {
+            finalScore = timer +1;
+            timeInt = clearInterval(timeInt);
+            quizScreen.remove();
+            addScore();
+        }
+        else if (id == answerKey[index]) {
+            var correct = document.createElement("h2");
+            correct.textContent = "CORRECT!!!";
+            quizScreen.appendChild(correct);
+            var timeout = setTimeout(function(){
+                quizScreen.innerHTML = "";
+                index++;
+                quiz(questions[index], answers[index],);
+            }, 1000);
+        } else {
+            var incorrect = document.createElement("h2");
+            incorrect.textContent = "WRONG!!!";
+            quizScreen.appendChild(incorrect);
+            timer -= 10;
+            var timeout = setTimeout(function(){
+                quizScreen.innerHTML = "";
+                index++;
+                quiz(questions[index], answers[index],);
+            }, 1000);
+        }
+    }
     else if (targetEl.matches(".btn")) {
-        console.log("high score!");
+        mainPage.remove();
+        quizScreen.remove();
+        showHigh();
+    }
+    else if (targetEl.matches(".return")) {
+        mainPage.remove();
+        highScoreScreen.remove();
+        quizScreen.remove();
+        highScoreScreen.remove();
+        inputScreen.remove();
+        frontPage();
+
     }
 }
-var quizHandler = function() {
-    for(var i = 0; i < questions.length; i++) {
-        quiz(questions[i], answers[i], answerKey[i]);
-    }
-}
-var quiz = function(q, a , ak) {
-    var quizScreen = document.createElement("div");
+var quiz = function(q, a) {
+    var counter = 0;
     quizScreen.className = "quiz";
     var questionTitle = document.createElement("h1");
     questionTitle.textContent = q;
@@ -66,13 +113,64 @@ var quiz = function(q, a , ak) {
         var listEl = document.createElement("li");
         var button = document.createElement("button");
         button.textContent = a[i];
+        button.setAttribute("data-answer-id", counter)
         listEl.appendChild(button);
         answersButtons.appendChild(listEl);
+        counter++;
     }
     quizScreen.appendChild(questionTitle);
     quizScreen.appendChild(answersButtons);
     body.appendChild(quizScreen);
 };
+var addScore = function() {
+    var initial = document.createElement("form");
+    var label = document.createElement("h1");
+    label.setAttribute("for", "inital");
+    label.textContent = "Enter Initials";
+    initial.appendChild(label);
+    var input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("placeholder", "Your Initials");
+    input.setAttribute("id", "inital");
+    initial.appendChild(input);
+    inputScreen.appendChild(initial);
+    body.appendChild(inputScreen);
+
+}
+var getIntial = function () {
+    var finalIntial = document.getElementById("inital").value;
+    initials.push(finalIntial);
+    saveInitals();
+    showHigh();
+}
+var saveInitals = function() {
+    if (initials in localStorage) {
+        console.log("in")
+    }
+    localStorage.setItem(initials, JSON.stringify(finalScore));
+}
+var showHigh = function() {
+    for (var i = 0; i < localStorage.length; i++) {
+        var initial = document.createElement("h1");
+        var score = document.createElement("h2");
+        var init = localStorage.key(i);
+        var personalScore = localStorage[init];
+        initial.textContent = init;
+        score.textContent = personalScore;
+        highScoreScreen.appendChild(initial);
+        highScoreScreen.appendChild(score);
+    }
+    var returnBtn = document.createElement("button");
+    returnBtn.className = "return"
+    returnBtn.textContent = "Return!";
+    highScoreScreen.appendChild(returnBtn);
+    body.appendChild(highScoreScreen);
+}
+var clear = function() {
+    localStorage.clear();
+}
 time();
 frontPage();
+//showHigh();
 body.addEventListener("click", buttonHandler);
+body.addEventListener("submit", getIntial);
